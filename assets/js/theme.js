27a0c063ae26e553043d482b0b15cb4853d0cc9c
Daniel Kling <dan@717studios.com>
2015-12-06 11:17:45 -0500

    initial commit

---
+++ assets/js/theme.js
@@ -0,0 +1,30 @@
+/**
+ * THEME JAVASCRIPT
+ */
+
+/**
+ * Imports
+ * 
+ * place all require statements here using relative links
+ * or npm module names. jQuery is an example below. both
+ * styles of naming conventions are used just in case
+ */
+global.jQuery = require( '../../bower_components/jquery/dist/jquery.min.js' );
+var $ = jQuery;
+var owlcar = require('../../node_modules/tappy-js/tappy.js');
+
+// var owlcar = require('../../bower_components/owlcar/owl-carousel/owl.carousel.js');
+
+// other examples
+//var foo = require( './foo.js' );
+//var bar = require( 'bar' );
+
+/**
+ * Place Your Code Here
+ */
+
+
+$(document).ready(function() {
+
+
+});
